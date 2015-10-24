5de7cde54e2328240fb26147fd9403f1c625ee84
RicardoACS <carrasco10@gmail.com>
2015-10-24 04:54:11 -0300

    Mantenedor registro equipo listo

---
+++ Negocio/ComunasNegocio.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datos;
+namespace Negocio
+{
+    [System.ComponentModel.DataObject]
+    public class ComunasNegocio
+    {
+        Datos.ClubExceptionTableAdapters.comunasTableAdapter comunas;
+
+        public ComunasNegocio()
+        {
+            comunas = new Datos.ClubExceptionTableAdapters.comunasTableAdapter();
+        }
+        [System.ComponentModel.DataObjectMethod(System.ComponentModel.DataObjectMethodType.Select, true)]
+        public ClubException.comunasDataTable mostrarComunas()
+        {
+            return comunas.GetData();
+        }
+    }
+}
---
+++ Negocio/Equipos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datos;
+namespace Negocio
+{
+    [System.ComponentModel.DataObject]
+    public class Equipos
+    {
+        Datos.ClubExceptionTableAdapters.equiposTableAdapter equipo;
+
+        public Equipos()
+        {
+            equipo = new Datos.ClubExceptionTableAdapters.equiposTableAdapter();
+        }
+
+        [System.ComponentModel.DataObjectMethod(System.ComponentModel.DataObjectMethodType.Insert, true)]
+        public int guardarEquipo(int idEquipo, string nombreEquipo, int idComuna, int rutDT, string division)
+        {
+            return equipo.Insert(idEquipo, nombreEquipo, idComuna, rutDT, 0, division);
+        }
+        [System.ComponentModel.DataObjectMethod(System.ComponentModel.DataObjectMethodType.Select, true)]
+        public ClubException.equiposDataTable mostrarEquipo()
+        {
+            return equipo.GetData();
+        }
+    }
+}
