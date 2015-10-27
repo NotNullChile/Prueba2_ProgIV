0796cdecff580e7350699c1812b260fc6a735713
rurtubiac <randy.urtubia@inacapmail.cl>
2015-10-26 21:39:07 -0300

    loginbox

--- WebApp1/Site1.Master.designer.cs
+++ WebApp1/Site1.Master.designer.cs
@@ -31,6 +31,24 @@ namespace WebApp1 {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// txt_username control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_username;
+        
+        /// <summary>
+        /// btn_login control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_login;
+        
+        /// <summary>
         /// nav1 control.
         /// </summary>
         /// <remarks>
